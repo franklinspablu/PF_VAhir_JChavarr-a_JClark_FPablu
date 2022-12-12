affb2d25fc2cffde3dd68a542cb3a01f228968c7
franklinspablu <franklinspablu@gmail.com>
2022-12-12 16:17:26 -0500

    MainActivity

--- app/src/main/java/com/example/semestral_hpa/MainActivity.java
+++ app/src/main/java/com/example/semestral_hpa/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.semestral_hpa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_asistencia);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void PasarLista(View v){
+        startActivity(new Intent(this,AsistenciaActivity.class));
+    }
+
+    public void VerLista(View v){
+        startActivity(new Intent(this,ListadoAsistenciaActivity.class));
     }
 }
