0a9bfb0ac14539a26beec144528ed9fd3c1b9f18
franklinspablu <franklinspablu@gmail.com>
2022-12-12 19:01:18 -0500

    Asistencia Activity

---
+++ app/src/main/java/com/example/semestral_hpa/AsistenciaActivity.java
@@ -0,0 +1,91 @@
+package com.example.semestral_hpa;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.semestral_hpa.Adapters.Estudiantes;
+import com.example.semestral_hpa.Helpers.Archivos;
+
+public class AsistenciaActivity extends AppCompatActivity {
+
+    NfcAdapter nfcAdapter;
+    PendingIntent mPendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asistencia);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null){
+            Toast.makeText(this,"NO tiene NFC io!",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        mPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,
+                getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_MUTABLE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onNewIntent(Intent intent) {
+        getTagInfo(intent);
+        super.onNewIntent(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void getTagInfo(Intent intent) {
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        String techList = tag.getTechList()[2];
+        if (techList.equals(Ndef.class.getName())){
+            Ndef mNdef = Ndef.get(tag);
+            if (mNdef!= null) {
+                NdefMessage mNdefMessage = mNdef.getCachedNdefMessage();
+                NdefRecord[] records = mNdefMessage.getRecords();
+                if(records != null){
+                    Estudiantes estudiante = new Estudiantes(
+                            new String(mNdefMessage.getRecords()[0].getPayload()),
+                            "",
+                            new String(mNdefMessage.getRecords()[1].getPayload()),
+                            "12:00 md"
+                    );
+
+                    new Archivos(getApplicationContext()).CrearArchivo("Asistencia20221LS142.txt",estudiante.toString());
+                }
+
+
+                String cedula = new String(mNdefMessage.getRecords()[0].getPayload());
+
+                Toast.makeText(getApplicationContext(),"Lectura => "+cedula,Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
